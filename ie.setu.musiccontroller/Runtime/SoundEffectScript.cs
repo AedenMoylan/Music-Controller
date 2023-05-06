fa4728ae7ad766f5507dc1e7783884cfeb0195bb
AedenMoylan <58327062+AedenMoylan@users.noreply.github.com>
2023-05-06 15:48:09 +0100

    Adding simple Sound effect Controller [YH-1]

---
+++ ie.setu.musiccontroller/Runtime/SoundEffectScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectScript : MonoBehaviour
+{
+    public AudioSource SoundEffect;
+    public string keyForAudioLowercase;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyForAudioLowercase))
+        {
+            AudioClip clip = SoundEffect.clip;
+            SoundEffect.PlayOneShot(clip);
+        }
+    }
+}
